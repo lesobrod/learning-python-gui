7725b19db61d5211bf3bed62d37c548deecb71e0
Denis <lesobrod@yandex.ru>
2022-02-09 15:56:38 +0300

    Version 1.4

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-"""
-
-
-"""
-
 class ResponseError(Exception):
-    """Not fatal errors of requests"""
+    """Non-fatal errors of requests"""
     pass
 
+
 GEOPOS_API_KEY = '625198479955388839225x120179'
+GUI_SIZE = "700x500"
+GUI_TITLE = "What are seasons?"
+MONTHS = ('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+          'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+          )
--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from matplotlib.figure import Figure
-from request_api import get_data
-from plot import plot_data
-from config import ResponseError
+from gui import show_gui
 
-
-try:
-    data = get_data(2016,'Panama')
-except ResponseError as e:
-    print(e)
-else:
-    plot_data(data)
-
+show_gui()
